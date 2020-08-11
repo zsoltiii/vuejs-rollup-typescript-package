d1837a1d22e19ef5df679e00d601f0b6f72f0b04
zsoltiii <zsoltiii@gmail.com>
2020-08-11 11:29:35 +0200

    Build

---
+++ dist/myLib.umd.js
@@ -0,0 +1,210 @@
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('vue-property-decorator')) :
+    typeof define === 'function' && define.amd ? define(['exports', 'vue-property-decorator'], factory) :
+    (global = global || self, factory(global.myLibrary = {}, global.vuePropertyDecorator));
+}(this, function (exports, vuePropertyDecorator) { 'use strict';
+
+    /*! *****************************************************************************
+    Copyright (c) Microsoft Corporation. All rights reserved.
+    Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+    this file except in compliance with the License. You may obtain a copy of the
+    License at http://www.apache.org/licenses/LICENSE-2.0
+
+    THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+    KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+    WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+    MERCHANTABLITY OR NON-INFRINGEMENT.
+
+    See the Apache Version 2.0 License for specific language governing permissions
+    and limitations under the License.
+    ***************************************************************************** */
+    /* global Reflect, Promise */
+
+    var extendStatics = function(d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+
+    function __extends(d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    }
+
+    function __decorate(decorators, target, key, desc) {
+        var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+        else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+        return c > 3 && r && Object.defineProperty(target, key, r), r;
+    }
+
+    var CoBtnComponent = /** @class */ (function (_super) {
+        __extends(CoBtnComponent, _super);
+        function CoBtnComponent() {
+            return _super !== null && _super.apply(this, arguments) || this;
+        }
+        CoBtnComponent = __decorate([
+            vuePropertyDecorator.Component({
+                name: 'co-btn'
+            })
+        ], CoBtnComponent);
+        return CoBtnComponent;
+    }(vuePropertyDecorator.Vue));
+
+    function normalizeComponent(template, style, script, scopeId, isFunctionalTemplate, moduleIdentifier
+    /* server only */
+    , shadowMode, createInjector, createInjectorSSR, createInjectorShadow) {
+      if (typeof shadowMode !== 'boolean') {
+        createInjectorSSR = createInjector;
+        createInjector = shadowMode;
+        shadowMode = false;
+      } // Vue.extend constructor export interop.
+
+
+      var options = typeof script === 'function' ? script.options : script; // render functions
+
+      if (template && template.render) {
+        options.render = template.render;
+        options.staticRenderFns = template.staticRenderFns;
+        options._compiled = true; // functional template
+
+        if (isFunctionalTemplate) {
+          options.functional = true;
+        }
+      } // scopedId
+
+
+      if (scopeId) {
+        options._scopeId = scopeId;
+      }
+
+      var hook;
+
+      if (moduleIdentifier) {
+        // server build
+        hook = function hook(context) {
+          // 2.3 injection
+          context = context || // cached call
+          this.$vnode && this.$vnode.ssrContext || // stateful
+          this.parent && this.parent.$vnode && this.parent.$vnode.ssrContext; // functional
+          // 2.2 with runInNewContext: true
+
+          if (!context && typeof __VUE_SSR_CONTEXT__ !== 'undefined') {
+            context = __VUE_SSR_CONTEXT__;
+          } // inject component styles
+
+
+          if (style) {
+            style.call(this, createInjectorSSR(context));
+          } // register component module identifier for async chunk inference
+
+
+          if (context && context._registeredComponents) {
+            context._registeredComponents.add(moduleIdentifier);
+          }
+        }; // used by ssr in case component is cached and beforeCreate
+        // never gets called
+
+
+        options._ssrRegister = hook;
+      } else if (style) {
+        hook = shadowMode ? function () {
+          style.call(this, createInjectorShadow(this.$root.$options.shadowRoot));
+        } : function (context) {
+          style.call(this, createInjector(context));
+        };
+      }
+
+      if (hook) {
+        if (options.functional) {
+          // register for functional component in vue file
+          var originalRender = options.render;
+
+          options.render = function renderWithStyleInjection(h, context) {
+            hook.call(context);
+            return originalRender(h, context);
+          };
+        } else {
+          // inject component registration as beforeCreate hook
+          var existing = options.beforeCreate;
+          options.beforeCreate = existing ? [].concat(existing, hook) : [hook];
+        }
+      }
+
+      return script;
+    }
+
+    var normalizeComponent_1 = normalizeComponent;
+
+    /* script */
+    var __vue_script__ = CoBtnComponent;
+
+    /* template */
+    var __vue_render__ = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('button',[_vm._v("asasdasdad")])};
+    var __vue_staticRenderFns__ = [];
+
+      /* style */
+      var __vue_inject_styles__ = undefined;
+      /* scoped */
+      var __vue_scope_id__ = undefined;
+      /* module identifier */
+      var __vue_module_identifier__ = undefined;
+      /* functional template */
+      var __vue_is_functional_template__ = false;
+      /* style inject */
+      
+      /* style inject SSR */
+      
+
+      
+      var CoBtn = normalizeComponent_1(
+        { render: __vue_render__, staticRenderFns: __vue_staticRenderFns__ },
+        __vue_inject_styles__,
+        __vue_script__,
+        __vue_scope_id__,
+        __vue_is_functional_template__,
+        __vue_module_identifier__,
+        undefined,
+        undefined
+      );
+
+
+
+    var components = /*#__PURE__*/Object.freeze({
+        CoBtn: CoBtn
+    });
+
+    // Import vue components
+    // install function executed by Vue.use()
+    function install(Vue) {
+        //@ts-ignore
+        if (install.installed)
+            { return; }
+        //@ts-ignore
+        install.installed = true;
+        Object.keys(components).forEach(function (componentName) {
+            Vue.component(componentName, components[componentName]);
+        });
+    }
+    // Create module definition for Vue.use()
+    var plugin = {
+        install: install,
+    };
+    // To auto-install when vue is found
+    /* global window global */
+    var GlobalVue = null;
+    if (typeof window !== 'undefined') {
+        GlobalVue = window.Vue;
+    }
+    if (GlobalVue) {
+        GlobalVue.use(plugin);
+    }
+
+    exports.default = plugin;
+    exports.CoBtn = CoBtn;
+
+    Object.defineProperty(exports, '__esModule', { value: true });
+
+}));
